54c5d8f7102012bf15a746cffbd3ecddbd119026
Faiz Akram <faiz2.techflux@gmail.com>
2016-11-17 13:09:08 +0530

    Added active admin

---
+++ app/admin/tracker.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register Tracker do
+
+filter :user
+filter :task
+filter :date
+
+index do
+# User.columns(:name)
+
+column :user  
+column :task
+column :hours
+column :date
+actions
+end
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+
+end
