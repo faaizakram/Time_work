9774cb47d449d1b7ed0f87ca50f65707d0b8013a
Faiz Akram <faiz2.techflux@gmail.com>
2016-11-22 12:36:59 +0530

    email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,16 +2,16 @@ class ApplicationMailer < ActionMailer::Base
 
 	# default from: "faiz2.techflux@gmail.com"
   
-  def confirmation_instructions(record)
-    devise_mail(record, :confirmation_instructions)
-  end
+  # def confirmation_instructions(record)
+  #   devise_mail(record, :confirmation_instructions)
+  # end
 
-  def reset_password_instructions(record)
-    devise_mail(record, :reset_password_instructions)
-  end
+  # def reset_password_instructions(record)
+  #   devise_mail(record, :reset_password_instructions)
+  # end
 
-  def unlock_instructions(record)
-    devise_mail(record, :unlock_instructions)
-  end
+  # def unlock_instructions(record)
+  #   devise_mail(record, :unlock_instructions)
+  # end
 
 end
