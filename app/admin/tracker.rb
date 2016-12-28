26a0530b194978b45e0a3c23c28e2db7dedb925f
Faiz Akram <faiz2.techflux@gmail.com>
2016-12-28 20:27:15 +0530

    bug fixes

--- app/admin/tracker.rb
+++ app/admin/tracker.rb
@@ -6,64 +6,27 @@ ActiveAdmin.register Tracker do
 
 
 	index do
-<<<<<<< HEAD
-	# 	column "Approve / Reject" do 
- #  link_to("Approve / Reject")
- # end
-=======
-		column "Approve / Reject" do 
-  link_to("Approve / Reject")
-end
->>>>>>> 009768c11490f0b56a250f347770391d81542793
 
+		column "Approve / Reject" do 
+        link_to("Approve / Reject")
+        end
         total = 0
 		column :user  
 		column :task
-<<<<<<< HEAD
-		column("Subtask Hours") {|resource| total = total + resource.total}
+        column("Subtask Hours") {|resource| total = total + resource.total}
 	    column :"Date", :created_at
-	   
-          actions
+	    actions
 	    div :class => "panel" do
-	       h3 "Total: #{total}"
-	    end
-	   # #  column :Subtrackers do |i|
-	   # #  columns  i.subtrackers.status
-	   # # end  
-    # end
+	        h3 "Total: #{total}"
+	     end
+	 #   # #  column :Subtrackers do |i|
+	 #   # #  columns  i.subtrackers.status
+	 #   # # end  
+     # end
   
-  end
 end
 	
-=======
-		column :subtask
-		column :hours
-	    column :"Date", :created_at
-	    column("sub amount") {|resource| total = total + resource.total}
-         actions
-	    div :class => "panel" do
-	      h3 "Total: #{total}"
-	    end
-	   #  column :Subtrackers do |i|
-	   #  columns  i.subtrackers.status
-	   # end  
 
-
-  form do |f|
-  	f.input do
-  		f.input :task
-  	end
-   
-   f.input "Subtasks" do
-   	f.input :subtrackers do |j|
-     j.input :task, :subtask
-    end
- end
->>>>>>> 009768c11490f0b56a250f347770391d81542793
-
-
-    end
-  end
 end
 	
 
